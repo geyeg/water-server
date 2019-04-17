00ce5cc3a292be637997749474cef093cffc1188
geyeg <geyeg@126.com>
2019-04-17 16:37:05 +0800

    remove is_send_to_sh...

--- server/config.py
+++ server/config.py
@@ -4,15 +4,14 @@
 __author__ = 'geyeg'
 
 HOST = '120.76.72.84'
-PORT = 8400
+PORT = 8015
 ip = '120.76.72.84'
-port = 8400
+port = 8015
 TIME_OUT = 240
 
 API_BASE_URL = r'http://182.61.56.51'
 POST_RETRY_INTERVAL = 3
-is_send_to_sh = False
-log_file = '/var/log/amwares/8400/water'
+log_file = '/var/log/amwares/8015/water'
 timezone = 8
 
 instruction_translation_dict = {
