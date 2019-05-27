0e35b4702fe54edf9001730eb597f61621b93a45
geyeg <geyeg@126.com>
2019-05-27 16:16:50 +0800

    add no_use_fir_fin_flag and standard_length

--- server/config.py
+++ server/config.py
@@ -17,3 +17,9 @@ timezone = 8
 instruction_translation_dict = {
     'upload_single': 'upload_single_lora'
 }
+
+# 如果协议中FIR、FIN位没正确使用，设为True(目前只针对宁波水表)
+NO_USE_FIR_FIN_FLAG = False
+
+# 标准长度为True时按协议中的算法 L = length(控制域+地址域+链路用户数据) * 4 + 1），False直接填入真实长度
+standard_length = True
